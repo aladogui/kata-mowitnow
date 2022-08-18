3ef3a0fbbe37f8999fe63e0e81f9b424c9b4f842
Ala <ala.dogui.ext@eulerhermes.com>
2022-08-18 21:38:24 +0200

    [Fifth iteration] Add GardenLimit model

--- src/main/java/com/hito/soc/gen/mowitnow/GardenLimit.java
+++ src/main/java/com/hito/soc/gen/mowitnow/GardenLimit.java
@@ -1,8 +1,26 @@
 package com.hito.soc.gen.mowitnow;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class GardenLimit {
 
   private Coordinates limitCoordinates;
+
+  public GardenLimit(int x , int y) {
+    limitCoordinates = new Coordinates(x,y);
+  }
+
+  public GardenLimit(String x, String y) {
+    try {
+      int xLimit= Integer.parseInt(x);
+      int yLimit = Integer.parseInt(y);
+      this.limitCoordinates = new Coordinates(xLimit, yLimit);
+    }catch(NumberFormatException ne){
+      log.error("Cannot parse garden limit coordinates parameters");
+      throw ne;
+    }
+  }
 
   public Coordinates getLimitCoordinates() {
     return limitCoordinates;
